f6df245d0bce472549681fa5ded64eb4ca264075
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-12-02 07:07:00 +0800

    Implementing the FakeAuctionServer

--- src/test/java/io/benjamintan/goos/FakeAuctionServer.java
+++ src/test/java/io/benjamintan/goos/FakeAuctionServer.java
@@ -1,9 +1,72 @@
 package io.benjamintan.goos;
 
-public class FakeAuctionServer {
-    private Object itemId;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 
-    public Object getItemId() {
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class FakeAuctionServer {
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String XMPP_HOSTNAME    = "localhost";
+    public static final String AUCTION_PASSWORD = "auction";
+
+    private final String itemId;
+    private final XMPPConnection connection;
+    private Chat currentChat;
+
+    private final SingleMessageListener messageListener = new SingleMessageListener();
+
+    public FakeAuctionServer(String itemId) {
+        this.itemId = itemId;
+        this.connection = new XMPPConnection(XMPP_HOSTNAME);
+    }
+
+    public void startSellingItem() throws XMPPException {
+        connection.connect();
+        connection.login(format(ITEM_ID_AS_LOGIN, itemId),
+            AUCTION_PASSWORD, AUCTION_RESOURCE);
+
+        connection.getChatManager().addChatListener((chat, createdLocally) -> {
+           currentChat = chat;
+        });
+    }
+
+    public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+        messageListener.receivesAMessage();
+    }
+
+    public void announceClosed() throws XMPPException {
+        currentChat.sendMessage(new Message());
+    }
+
+    public void stop() {
+        connection.disconnect();
+    }
+
+    public String getItemId() {
         return itemId;
+    }
+
+    public class SingleMessageListener implements MessageListener {
+        private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<>(1);
+
+        @Override
+        public void processMessage(Chat chat, Message message) {
+            messages.add(message);
+        }
+
+        public void receivesAMessage() throws InterruptedException {
+            assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+        }
     }
 }

3eb2cf2651131b8d49e9334efe5eefc3181615d3
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-12-06 23:13:47 +0800

    From JLabel to JTable

--- src/main/java/io/benjamintan/goos/MainWindow.java
+++ src/main/java/io/benjamintan/goos/MainWindow.java
@@ -2,9 +2,12 @@ package io.benjamintan.goos;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private static final String SNIPERS_TABLE_NAME = "Snipers";
+    private final SnipersTableModel snipers = new SnipersTableModel();
 
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     public static final String SNIPER_STATUS_NAME = "sniper status";
@@ -13,16 +16,30 @@ public class MainWindow extends JFrame {
     public static final String STATUS_BIDDING = "bidding";
     public static final String STATUS_WINNING = "winning";
     public static final String STATUS_WON = "won";
+    public static final String APPLICATION_NAME = "Auction Sniper";
 
     private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
     public MainWindow() {
-        super("Auction Sniper");
+        super(APPLICATION_NAME);
         setName(MAIN_WINDOW_NAME);
-        add(sniperStatus);
+        fillContentPane(makeSnipersTable());
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private void fillContentPane(JTable snipersTable) {
+        final Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+    }
+
+    private JTable makeSnipersTable() {
+        final JTable snipersTable = new JTable(snipers);
+        snipersTable.setName(SNIPERS_TABLE_NAME);
+        return snipersTable;
     }
 
     private static JLabel createLabel(String initialText) {
@@ -32,7 +49,31 @@ public class MainWindow extends JFrame {
         return result;
     }
 
-    public void showStatus(String status) {
-        sniperStatus.setText(status);
+    public void showStatusText(String statusText) {
+        snipers.setStatusText(statusText);
+    }
+
+    private class SnipersTableModel extends AbstractTableModel {
+        private String statusText = STATUS_JOINING;
+
+        @Override
+        public int getRowCount() {
+            return 1;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return statusText;
+        }
+
+        public void setStatusText(String newStatusText) {
+            statusText = newStatusText;
+            fireTableRowsUpdated(0, 0);
+        }
     }
 }
--- src/test/java/io/benjamintan/goos/AuctionSniperDriver.java
+++ src/test/java/io/benjamintan/goos/AuctionSniperDriver.java
@@ -3,8 +3,10 @@ package io.benjamintan.goos;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 
@@ -19,7 +21,6 @@ public class AuctionSniperDriver extends JFrameDriver {
     }
 
     public void showSniperStatus(String statusText) {
-        new JLabelDriver(
-            this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+        new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
     }
 }
